d425c4604d4abcf8955adc21cd8b0e4026552e7f
IFESINACHI <IFESINACHI@DESKTOP-KFCG7TC>
2022-05-09 00:15:31 +0100

    Add project files.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] names = { "Ifesi", "Vicky", "Nnadozie", "Giddy", "Iruoma", };
+            for (int i = names.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(@"The Last name in the array is " + names[i]);
+                Console.WriteLine($"The number of names in the array is " + (i));
+                break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] names = { "Ifesi", "Gideon", "Iruoma", "Victory" };
+            Console.WriteLine("The reversed names are: ");
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"{names[i]}");
+            }
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Normal order in the Array are: ");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
